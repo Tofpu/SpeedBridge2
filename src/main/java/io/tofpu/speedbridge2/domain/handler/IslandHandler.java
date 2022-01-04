3fbe44df6ed5134de1f91647a28fc4298a88d2c9
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-01-04 16:16:41 +0200

    initial commit

---
+++ src/main/java/io/tofpu/speedbridge2/domain/handler/IslandHandler.java
@@ -0,0 +1,14 @@
+package io.tofpu.speedbridge2.domain.handler;
+
+import io.tofpu.speedbridge2.domain.island.Island;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IslandHandler {
+    private final Map<Integer, Island> islands = new HashMap<>();
+
+    public void createIsland(final int slot) {
+        this.islands.putIfAbsent(slot, new Island(slot));
+    }
+}
