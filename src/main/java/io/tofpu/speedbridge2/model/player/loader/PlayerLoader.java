f00d36892d707af595c187d1a9a562bef48eed58
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-04-02 17:06:32 +0200

    Fixed the player expiration issue

--- src/main/java/io/tofpu/speedbridge2/model/player/loader/PlayerLoader.java
+++ src/main/java/io/tofpu/speedbridge2/model/player/loader/PlayerLoader.java
@@ -20,6 +20,10 @@ import java.util.concurrent.TimeUnit;
 public final class PlayerLoader implements BoardRetrieve<BridgePlayer>, CacheLoader<UUID, BridgePlayer> {
     public static final PlayerLoader INSTANCE = new PlayerLoader();
 
+    private PlayerLoader() {
+        // prevent instantiation
+    }
+
     @Override
     public BridgePlayer load(final @NotNull UUID key) throws Exception {
         return this.retrieve(key);
@@ -57,27 +61,45 @@ public final class PlayerLoader implements BoardRetrieve<BridgePlayer>, CacheLoa
 
     public static final class PlayerRemovalListener implements Expiry<UUID, BridgePlayer> {
         private static final long EXPIRY_DURATION = TimeUnit.MINUTES.toNanos(5);
+        private static final long INFINITE_DURATION = Long.MAX_VALUE;
+
         public static final PlayerRemovalListener INSTANCE = new PlayerRemovalListener();
+
+        private PlayerRemovalListener() {
+            // prevent instantiation
+        }
 
         @Override
         public long expireAfterCreate(final UUID key, final BridgePlayer value, final long currentTime) {
-            return Long.MAX_VALUE;
+            return EXPIRY_DURATION;
         }
 
         @Override
         public long expireAfterUpdate(final UUID key, final BridgePlayer value, final long currentTime,
                 @NonNegative final long currentDuration) {
-            BridgeUtil.debug("PlayerRemovalListener#expireAfterUpdate: Start: " + Duration.ofNanos(currentDuration).getSeconds());
+            final long elapse = Duration.ofNanos(currentDuration)
+                    .getSeconds();
+            BridgeUtil.debug("PlayerRemovalListener#expireAfterUpdate: Start: current " +
+                             "duration is " + elapse + " seconds!");
             if (value.getPlayer() == null) {
-                BridgeUtil.debug("PlayerRemovalListener#expireAfterUpdate: Expiring " + key + " player data after 5 minutes!");
+                BridgeUtil.debug("PlayerRemovalListener#expireAfterUpdate: Expiring " + key + " player data after " + elapse + " seconds!");
                 return EXPIRY_DURATION;
             }
-            return Long.MAX_VALUE;
+            BridgeUtil.debug("PlayerRemovalListener#expireAfterUpdate: Not expiring " + key + " player data yet!");
+            return INFINITE_DURATION;
         }
 
         @Override
         public long expireAfterRead(final UUID key, final BridgePlayer value, final long currentTime,
                 @NonNegative final long currentDuration) {
+            final long elapse = Duration.ofNanos(currentDuration)
+                    .getSeconds();
+            BridgeUtil.debug("PlayerRemovalListener#expireAfterRead: Start: " + elapse);
+            if (value.getPlayer() != null) {
+                BridgeUtil.debug("PlayerRemovalListener#expireAfterRead: Not expiring " + key + " player data yet!");
+                return INFINITE_DURATION;
+            }
+            BridgeUtil.debug("PlayerRemovalListener#expireAfterRead: Expiring " + key + " player data after " + elapse + " seconds!");
             return currentDuration;
         }
     }
