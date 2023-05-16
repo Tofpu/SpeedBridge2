1c98bc0c5df3bddab3f3c1669a56ec50ef583405
Tofpu <tofpuu@gmail.com>
2023-05-16 03:30:49 +0300

    Throw exception when creation of file fails

--- core/src/main/java/com/github/tofpu/speedbridge2/configuration/impl/BasicConfiguration.java
+++ core/src/main/java/com/github/tofpu/speedbridge2/configuration/impl/BasicConfiguration.java
@@ -4,6 +4,7 @@ import static com.github.tofpu.speedbridge2.util.ProgramCorrectness.requireState
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.LinkedHashMap;
@@ -34,17 +35,25 @@ public class BasicConfiguration {
         requireState(!fromFile.isDirectory(), "This %s must be a file, not a directory!",
             fromFile.getPath());
         if (!fromFile.exists()) {
-            File parentFile = fromFile.getParentFile();
-            if (!parentFile.exists()) {
-                Files.createDirectories(parentFile.toPath());
-            }
-            Files.createFile(fromFile.toPath());
+            createFile(fromFile);
+        }
+
+        if (!fromFile.exists()) {
+            throw new IllegalStateException("Unable to create file: " + fromFile.getName());
         }
 
         Map<String, Object> loadedMap = this.yaml.load(new FileInputStream(fromFile));
         if (loadedMap != null && loadedMap.size() != 0) {
             this.objectMap.putAll(loadedMap);
         }
+    }
+
+    private static void createFile(File fromFile) throws IOException {
+        File parentFile = fromFile.getParentFile();
+        if (!parentFile.exists()) {
+            Files.createDirectories(parentFile.toPath());
+        }
+        Files.createFile(fromFile.toPath());
     }
 
     public void set(final String key, final Object obj) {
