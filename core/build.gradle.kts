21e63069e0900e2a9904c7c838eaae0f98456d8a
Tofpu <tofpuu@gmail.com>
2023-09-24 16:20:16 +0300

    Fixes a bug where config.yml would be locked for modification

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
     api("org.reflections:reflections:0.10.2")
     api("com.h2database:h2:2.1.214")
     api("org.yaml:snakeyaml:2.0")
-    api("com.github.Tofpu:dynamic-configuration:1.0.1")
+    api("com.github.Tofpu:dynamic-configuration:1.0.2")
     api("org.spongepowered:configurate-yaml:4.0.0")
     implementation("com.mysql:mysql-connector-j:8.1.0")
 }
