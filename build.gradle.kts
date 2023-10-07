90fb5b7721704d39deff9afbcd3e71a073f8b626
Tofpu <tofpuu@gmail.com>
2023-10-07 20:39:43 +0300

    Add groupId and version to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ subprojects {
         maven("https://jitpack.io/")
     }
 
+    group = "com.github.tofpu.speedbridge2"
+    version = "2.0.0-alpha-" + "git rev-parse --short=8 HEAD".runCommand(rootDir)
+
     dependencies {
         val implementation by configurations
         implementation("org.jetbrains:annotations:24.0.0")
@@ -35,3 +38,21 @@ subprojects {
         this.sourceCompatibility = JavaVersion.VERSION_1_8
     }
 }
+
+fun String.runCommand(
+        workingDir: File = File("."),
+        timeoutAmount: Long = 60,
+        timeoutUnit: TimeUnit = TimeUnit.SECONDS
+): String = ProcessBuilder(split("\\s(?=(?:[^'\"`]*(['\"`])[^'\"`]*\\1)*[^'\"`]*$)".toRegex()))
+        .directory(workingDir)
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start()
+        .apply { waitFor(timeoutAmount, timeoutUnit) }
+        .run {
+            val error = errorStream.bufferedReader().readText().trim()
+            if (error.isNotEmpty()) {
+                throw Exception(error)
+            }
+            inputStream.bufferedReader().readText().trim()
+        }
