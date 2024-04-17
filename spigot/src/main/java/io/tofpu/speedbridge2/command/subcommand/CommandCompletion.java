90e8adc3c58861e0461e01ef6c2a6e38c2c07e8c
Tofpu <tofpuu@gmail.com>
2024-04-17 05:59:54 +0200

    Exclude sender from autocompletion of online players

--- spigot/src/main/java/io/tofpu/speedbridge2/command/subcommand/CommandCompletion.java
+++ spigot/src/main/java/io/tofpu/speedbridge2/command/subcommand/CommandCompletion.java
@@ -3,7 +3,10 @@ package io.tofpu.speedbridge2.command.subcommand;
 import io.tofpu.speedbridge2.command.condition.annotation.MaterialType;
 import io.tofpu.speedbridge2.model.island.IslandService;
 import io.tofpu.speedbridge2.util.material.MultiMaterial;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.HumanEntity;
 import org.jetbrains.annotations.NotNull;
+import revxrsal.commands.bukkit.BukkitCommandActor;
 import revxrsal.commands.command.CommandActor;
 import revxrsal.commands.command.ExecutableCommand;
 
@@ -12,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public final class CommandCompletion {
     private final IslandService islandService;
@@ -21,7 +25,7 @@ public final class CommandCompletion {
     }
 
     public @NotNull List<String> islands(final List<String> args, final CommandActor actor,
-            final ExecutableCommand command) {
+                                         final ExecutableCommand command) {
         final List<String> suggestions = new ArrayList<>();
 
         for (final Integer integer : islandService.getIntegerIslands()) {
@@ -49,5 +53,15 @@ public final class CommandCompletion {
             return MultiMaterial.materials();
         }
         return MultiMaterial.materials(annotation.category());
+    }
+
+    public Collection<String> players(@NotNull List<String> args, @NotNull CommandActor sender, @NotNull ExecutableCommand executableCommand) {
+        return Bukkit.getOnlinePlayers().stream()
+                .filter(player -> !((BukkitCommandActor) sender).isPlayer()
+                        || ((BukkitCommandActor) sender).requirePlayer().canSee(player)
+                        && !((BukkitCommandActor) sender).getSender().equals(player)
+                ).map(HumanEntity::getName)
+                .collect(Collectors.toList());
+
     }
 }
