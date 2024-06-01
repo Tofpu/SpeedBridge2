05e4423b2974c0deffeb924b2a0051509543a68f
Tofpu <tofpuu@gmail.com>
2024-06-01 14:47:52 +0300

    Add a config option for not clearing player's inventory on join

--- spigot/src/main/java/io/tofpu/speedbridge2/model/common/config/category/LobbyCategory.java
+++ spigot/src/main/java/io/tofpu/speedbridge2/model/common/config/category/LobbyCategory.java
@@ -8,16 +8,21 @@ import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 import java.util.concurrent.CompletableFuture;
 
+@SuppressWarnings("FieldCanBeLocal")
 @ConfigSerializable
 public final class LobbyCategory {
     @Setting("teleport-on-join")
     @Comment("If this is set to true, the players will teleport to the lobby upon them " +
             "joining")
     private boolean teleportOnJoin = true;
+
     @Setting("lobby-location")
     @Comment("This is where the players will teleport when they join, or leave an " +
             "island")
     private Location lobbyLocation = null;
+
+    @Comment("If this is set to true, the player's inventory will be cleared after teleporting to the lobby upon joining")
+    private boolean clearInventoryAfterTeleport = true;
 
     public boolean isTeleportOnJoin() {
         return teleportOnJoin;
@@ -25,6 +30,10 @@ public final class LobbyCategory {
 
     public void setTeleportOnJoin(final boolean teleportOnJoin) {
         this.teleportOnJoin = teleportOnJoin;
+    }
+
+    public boolean clearInventoryAfterTeleport() {
+        return clearInventoryAfterTeleport;
     }
 
     public CompletableFuture<Void> setLobbyLocation(final Location lobbyLocation) {
