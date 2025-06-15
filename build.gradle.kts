1fd8448456fedad735a98c42d8e0982986dc722e
tofpu <tofpuu@gmail.com>
2025-06-15 17:33:37 +0300

    feat: bump jdk version to 17
    May revert back later if there's demand for it

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ subprojects {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(8))
+            languageVersion.set(JavaLanguageVersion.of(17))
         }
     }
 }
