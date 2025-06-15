4b27e05e9e5227bbe18a2bed2e46c88c5ab40376
tofpu <tofpuu@gmail.com>
2025-06-15 17:35:07 +0300

    feat: introduce system for island
    This class is responsible for initializing and wiring all the necessary components

---
+++ spigot/src/main/java/io/tofpu/speedbridge2/island/system/IslandSystem.java
@@ -0,0 +1,39 @@
+package io.tofpu.speedbridge2.island.system;
+
+import io.tofpu.speedbridge2.command.CommandHandler;
+import io.tofpu.speedbridge2.database.infra.db.Database;
+import io.tofpu.speedbridge2.island.command.IslandCommandHandler;
+import io.tofpu.speedbridge2.island.domain.IslandRepository;
+import io.tofpu.speedbridge2.island.persistence.IslandDao;
+import io.tofpu.speedbridge2.island.persistence.IslandMapper;
+import io.tofpu.speedbridge2.island.persistence.IslandRepositoryImpl;
+import io.tofpu.speedbridge2.island.service.IslandService;
+import io.tofpu.speedbridge2.schematic.SchematicService;
+
+public class IslandSystem {
+    private IslandService islandService;
+
+    public void load(Database database, SchematicService schematicService) {
+        IslandRepository repository = new IslandRepositoryImpl(
+                new IslandDao(database),
+                new IslandMapper(),
+                schematicService
+        );
+        islandService = new IslandService(repository);
+        islandService.load();
+    }
+
+    public void registerCommands(CommandHandler commandHandler) {
+        if (islandService == null) {
+            throw new IllegalStateException("IslandService is not initialized. Call load() first.");
+        }
+        IslandCommandHandler.init(commandHandler, islandService);
+    }
+
+    public IslandService islandService() {
+        if (islandService == null) {
+            throw new IllegalStateException("IslandService is not initialized. Call enable() first.");
+        }
+        return islandService;
+    }
+}
