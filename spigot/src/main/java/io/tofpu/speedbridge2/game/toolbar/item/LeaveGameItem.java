e9ad9256e20a6c61488478459880b819810ade39
tofpu <tofpuu@gmail.com>
2025-06-16 10:27:58 +0300

    fix: game tools not being triggered due to using wrong events

--- spigot/src/main/java/io/tofpu/speedbridge2/game/toolbar/item/LeaveGameItem.java
+++ spigot/src/main/java/io/tofpu/speedbridge2/game/toolbar/item/LeaveGameItem.java
@@ -4,7 +4,7 @@ import io.tofpu.speedbridge2.game.GameSupplier;
 import io.tofpu.speedbridge2.game.state.GameStateType;
 import io.tofpu.toolbar.toolbar.tool.action.ToolAction;
 import io.tofpu.toolbar.toolbar.tool.action.ToolActionUtil;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class LeaveGameItem extends GameItem {
@@ -12,10 +12,10 @@ public class LeaveGameItem extends GameItem {
     protected static final String ID = "leaveGame";
 
     public LeaveGameItem(ItemStack item, GameSupplier gameSupplier) {
-        super(ID, item, ToolActionUtil.listenFor(PlayerInteractAtEntityEvent.class, handle(gameSupplier)));
+        super(ID, item, ToolActionUtil.listenFor(PlayerInteractEvent.class, handle(gameSupplier)));
     }
 
-    private static ToolAction<PlayerInteractAtEntityEvent> handle(GameSupplier gameSupplier) {
+    private static ToolAction<PlayerInteractEvent> handle(GameSupplier gameSupplier) {
         return (toolbar, event) ->
                 gameSupplier.ifGamePresent(event.getPlayer().getUniqueId(), game -> game.setState(GameStateType.STOP));
     }
