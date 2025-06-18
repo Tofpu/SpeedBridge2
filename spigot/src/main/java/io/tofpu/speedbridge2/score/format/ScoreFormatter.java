788c372d6389acb99412cf0f00788b8fd328c3de
tofpu <tofpuu@gmail.com>
2025-06-18 14:51:41 +0300

    feat: introduce score formatter

---
+++ spigot/src/main/java/io/tofpu/speedbridge2/score/format/ScoreFormatter.java
@@ -0,0 +1,33 @@
+package io.tofpu.speedbridge2.score.format;
+
+import io.tofpu.speedbridge2.score.domain.Score;
+import io.tofpu.speedbridge2.score.format.rounding.ScoreRoundingHandler;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ScoreFormatter {
+    private final ScoreRoundingHandler roundingHandler;
+
+    public ScoreFormatter(ScoreRoundingHandler roundingHandler) {
+        this.roundingHandler = roundingHandler;
+    }
+
+    public String format(Score score) {
+        double roundedTime = roundingHandler.round(score.time());
+        return formatDuration(roundingHandler.formatDuration(roundedTime));
+    }
+
+    public String formatDuration(BigDecimal decimal) {
+        double value = decimal.doubleValue();
+        if (value < 60) {
+            return value + "s";
+        } else if (value < 3600) {
+            return decimal.divide(BigDecimal.valueOf(60), RoundingMode.DOWN).toPlainString() + "m";
+        } else if (value < 86400) {
+            return decimal.divide(BigDecimal.valueOf(3600), RoundingMode.DOWN) + "h";
+        } else {
+            return decimal.divide(BigDecimal.valueOf(86400), RoundingMode.DOWN) + "d";
+        }
+    }
+}
